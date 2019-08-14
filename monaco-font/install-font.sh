a6076007c2c6e078352b1a0fce6da1cdc19f7de7
Wesley <lixiaofan0122@gmail.com>
2019-08-14 15:26:12 +0800

    modify monaco font get mothods

--- monaco-font/install-font.sh
+++ monaco-font/install-font.sh
@@ -2,9 +2,10 @@
 
 echo "Start install"
 sudo mkdir -p /usr/share/fonts/truetype/custom
-echo "Downloading font"
-wget -c https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf
+#echo "Downloading font"
+#wget -c https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf
+#wget -c https://github.com/todylu/monaco.ttf/blob/master/monaco.ttf
 echo "Installing font"
-sudo mv Monaco_Linux.ttf /usr/share/fonts/truetype/custom/
+sudo cp monaco.ttf /usr/share/fonts/truetype/custom/
 sudo fc-cache -f -v
 echo "Enjoy"
