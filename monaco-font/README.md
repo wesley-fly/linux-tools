65cc2b99d35a464c11ba8b9a3f52e80997342f51
Wesley <lixiaofan0122@gmail.com>
2019-08-14 15:44:11 +0800

    Adjust monaco font

--- monaco-font/README.md
+++
@@ -1,8 +0,0 @@
-monaco-font
-===========
-
-Install Monaco Font on Ubuntu
-
-curl -kL https://raw.github.com/cstrap/monaco-font/master/install-font.sh | bash
-
-Inspired by http://jorrel.blogspot.it/2007/11/monaco-on-ubuntu.html
--- monaco-font/install-font.sh
+++ monaco-font/install-font.sh
@@ -2,10 +2,7 @@
 
 echo "Start install"
 sudo mkdir -p /usr/share/fonts/truetype/custom
-#echo "Downloading font"
-#wget -c https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf
-#wget -c https://github.com/todylu/monaco.ttf/blob/master/monaco.ttf
 echo "Installing font"
-sudo cp monaco.ttf /usr/share/fonts/truetype/custom/
+sudo cp Monaco_Linux.ttf /usr/share/fonts/truetype/custom/
 sudo fc-cache -f -v
 echo "Enjoy"
